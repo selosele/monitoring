2a45a7377647345a6d261bc042642201a1517096
selosele <osh3384@naver.com>
2025-05-10 04:18:36 +0000

    리팩토링

--- internal/sysinfo/service/service.go
+++ internal/sysinfo/service/service.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"monitoring/internal/sysinfo/types"
+	"monitoring/pkg/utils"
 
 	"github.com/shirou/gopsutil/v3/cpu"
 	"github.com/shirou/gopsutil/v3/disk"
@@ -12,36 +13,48 @@ import (
 )
 
 // 시스템정보를 가져오는 함수
-func GetSysInfo() (*types.SysInfo, error) {
+func GetSysInfo() *types.SysInfo {
 	hostStat, err := host.Info()
 	if err != nil {
 		fmt.Println("Error getting host info:", err)
-		return &types.SysInfo{}, err
+		return &types.SysInfo{}
 	}
 
 	cpuStat, err := cpu.Info()
 	if err != nil {
 		fmt.Println("Error getting cpu info:", err)
-		return &types.SysInfo{}, err
+		return &types.SysInfo{}
 	}
 
 	vmStat, err := mem.VirtualMemory()
 	if err != nil {
-		fmt.Println("Error getting memory info:", err)
-		return &types.SysInfo{}, err
+		fmt.Println("Error getting virtual memory info:", err)
+		return &types.SysInfo{}
+	}
+
+	smStat, err := mem.SwapMemory()
+	if err != nil {
+		fmt.Println("Error getting swap memory info:", err)
+		return &types.SysInfo{}
 	}
 
 	diskStat, err := disk.Usage("//")
 	if err != nil {
 		fmt.Println("Error getting disk info:", err)
-		return &types.SysInfo{}, err
+		return &types.SysInfo{}
 	}
 
-	return &types.SysInfo{
-		Hostname: hostStat.Hostname,
-		Platform: hostStat.Platform,
-		CPU:      cpuStat[0].ModelName,
-		RAM:      vmStat.Total / 1024 / 1024,
-		Disk:     diskStat.Total / 1024 / 1024,
-	}, nil
+	sysInfo := &types.SysInfo{
+		Hostname:    hostStat.Hostname,
+		Platform:    hostStat.Platform,
+		CPU:         cpuStat[0].ModelName,
+		RAM:         utils.BytesToGB(vmStat.Total),
+		RAMUsageOf:  fmt.Sprintf("%.2f%%", vmStat.UsedPercent),
+		Swap:        utils.BytesToGB(smStat.Total),
+		SwapUsageOf: fmt.Sprintf("%.2f%%", smStat.UsedPercent),
+		Disk:        utils.BytesToGB(diskStat.Total),
+		DiskUsageOf: fmt.Sprintf("%.2f%%", diskStat.UsedPercent),
+	}
+
+	return sysInfo
 }
--- internal/sysinfo/types/types.go
+++ internal/sysinfo/types/types.go
@@ -1,10 +1,30 @@
 package types
 
-// 시스템정보 구조체
+import "fmt"
+
+// 시스템 정보 구조체
 type SysInfo struct {
-	Hostname string
-	Platform string
-	CPU      string
-	RAM      uint64
-	Disk     uint64
+	Hostname    string  // 호스트 이름
+	Platform    string  // 플랫폼
+	CPU         string  // CPU 모델 이름
+	RAM         float64 // RAM 용량 (GB 단위)
+	RAMUsageOf  string  // RAM 사용량 비율 (퍼센트 단위)
+	Swap        float64 // 스왑 용량 (GB 단위)
+	SwapUsageOf string  // 스왑 사용량 비율 (퍼센트 단위)
+	Disk        float64 // 디스크 용량 (GB 단위)
+	DiskUsageOf string  // 디스크 사용량 비율 (퍼센트 단위)
+}
+
+// 시스템 정보를 출력하는 함수
+func (s SysInfo) Print() {
+	fmt.Println("■ SYSTEM INFO:")
+	fmt.Printf("> hostname: %+v\n", s.Hostname)
+	fmt.Printf("> platform: %+v\n", s.Platform)
+	fmt.Printf("> cpu: %+v\n", s.CPU)
+	fmt.Printf("> ram: %+vGB\n", s.RAM)
+	fmt.Printf("> ram usage of: %+v\n", s.RAMUsageOf)
+	fmt.Printf("> swap: %+vGB\n", s.Swap)
+	fmt.Printf("> swap usage of: %+v\n", s.SwapUsageOf)
+	fmt.Printf("> disk: %+vGB\n", s.Disk)
+	fmt.Printf("> disk usage of: %+v\n", s.DiskUsageOf)
 }
